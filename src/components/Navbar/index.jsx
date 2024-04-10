5333ec2a452786bc3a80a1765fc430b88d1bba6a
Aditya <104072819+Soumya2411@users.noreply.github.com>
2024-04-10 23:10:14 +0530

    fixed navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         {/* Mobile nav */}
         <ul
           className={`
-        md:hidden bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-24 pl-4
+        md:hidden z-10 bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-24 pl-4
         duration-500 ${open ? "left-0" : "left-[-100%]"}
         `}
         >
