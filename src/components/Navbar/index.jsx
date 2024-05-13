9d79239af1122476ecc8a116caf1b157b1d921e2
Aditya <adityaa@xhopfront.com>
2024-05-13 16:04:07 +0530

    updated navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,39 +1,51 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/img/Untitled-design-17.svg";
-import NavLinks from "./NavLinks";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const handleLinkClick = () => {
+    // Close the navbar when a link is clicked
+    setOpen(false);
+  };
   return (
     <nav className="bg-white">
       <div className="flex items-center font-medium justify-between md:mx-6">
         <div className="z-50  md:w-auto w-full flex justify-between">
           <Link to='https://www.caclouddesk.com'>
-          <img src={Logo} alt="logo" className="md:cursor-pointer " />
+            <img src={Logo} alt="logo" className="md:cursor-pointer " />
           </Link>
-          <div className="text-3xl md:hidden my-auto z-50" onClick={() => setOpen(!open)}>
+          <div className="text-3xl md:hidden my-auto z-50 mr-2" onClick={() => setOpen(!open)}>
             <ion-icon name={`${open ? "close" : "menu"}`}></ion-icon>
           </div>
         </div>
         <ul className="md:flex hidden items-center gap-8 font-[Poppins] text-sm font-semibold">
           <li>
-            <Link to="https://www.caclouddesk.com" className="py-7 px-3 inline-block">
-              Home
+            <Link to="/" className="py-7 px-3 inline-block">
+              GST Calculator
             </Link>
           </li>
           <li>
-            <Link to="https://www.caclouddesk.com/demo-help-videos/" className="py-7 px-3 inline-block">
-              Live Demo
+            <Link to="/taxpayersearch" className="py-7 px-3 inline-block">
+              GST Search / Taxpayer Details
             </Link>
           </li>
           <li>
-            <Link to="https://demo.caclouddesk.com/" className="py-7 px-3 inline-block">
-              Try Now
+            <Link to="/filinginfo" className="py-7 px-3 inline-block">
+              GST Filing Info
             </Link>
           </li>
-          <NavLinks />
+          <li>
+            <Link to="https://partner.caclouddesk.com" className="py-7 px-3 inline-block">
+              Partner Login
+            </Link>
+          </li>
+          <li>
+            <Link to="https://customer.caclouddesk.com" className="py-7 px-3 inline-block">
+              Client Login
+            </Link>
+          </li>
         </ul>
-        
+
         {/* Mobile nav */}
         <ul
           className={`
@@ -41,22 +53,31 @@ const Navbar = () => {
         duration-500 ${open ? "left-0" : "left-[-100%]"}
         `}
         >
-           <li>
-            <Link to="https://www.caclouddesk.com" className="py-7 px-3 inline-block">
-              Home
+          <li>
+            <Link to="/" className="py-7 px-3 inline-block" onClick={() => setOpen(!open)}>
+              GST Calculator
             </Link>
           </li>
           <li>
-            <Link to="https://www.caclouddesk.com/demo-help-videos/" className="py-7 px-3 inline-block">
-              Live Demo
+            <Link to="/taxpayersearch" className="py-7 px-3 inline-block" onClick={() => setOpen(!open)}>
+              GST Search / Taxpayer Details
             </Link>
           </li>
           <li>
-            <Link to="https://demo.caclouddesk.com/" className="py-7 px-3 inline-block">
-              Try Now
+            <Link to="/filinginfo" className="py-7 px-3 inline-block" onClick={() => setOpen(!open)}>
+              GST Filing Info
             </Link>
           </li>
-          <NavLinks />
+          <li>
+            <Link to="https://partner.caclouddesk.com" className="py-7 px-3 inline-block" onClick={() => setOpen(!open)}>
+              Partner Login
+            </Link>
+          </li>
+          <li>
+            <Link to="https://customer.caclouddesk.com" className="py-7 px-3 inline-block" onClick={() => setOpen(!open)}>
+              Client Login
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
