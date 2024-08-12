05bf3f3e1d406fa1b8eb7a92d46ea5c9b0df73d8
Raj <99405991+QMiNt@users.noreply.github.com>
2024-08-12 22:47:15 +0530

    chore: Update InfographicsDownloadPage with new color options, form input limits, image styling, button alignment, and responsiveness

---
+++ src/components/PreviewComponent.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/prop-types */
+const PreviewComponent = ({ imgLink, footerInfo, bgColor ,logo}) => {
+  return (
+    <div className="md:w-[540px] w-[360px] ">
+      <img
+        src={`https://utility.caclouddesk.com/uploads/${imgLink}`}
+        alt="Background"
+        className="w-full h-auto object-cover"
+      />
+      <div
+        className={`w-full h-[100px] ${bgColor ? '' : 'bg-black'} flex justify-around items-center`}
+        style={{ backgroundColor: bgColor || '#000' }}
+      >
+        <div className={`w-1/6 h-full flex justify-center items-center `}>
+          <img
+            src={logo || "https://as2.ftcdn.net/v2/jpg/04/78/56/33/1000_F_478563312_HuepEVbPHRGC0dsbXOXL1YSuFIkWEm2m.jpg"}
+            alt="Logo"
+            className="w-[90%] h-auto  rounded-full bg-white"
+          />
+        </div>
+        <div className="min-w-[50%] max-w-[80%] grid grid-cols-1 gap-y-1 text-white text-[16px]">
+          <div className={`flex items-center`}>
+            <i className="fa fa-user mr-2"></i>
+            <p>
+              <span>{footerInfo.name || 'Your Name'}</span>
+            </p>
+          </div>
+          <div className={`flex items-center`}>
+            <i className="fa fa-phone mr-2"></i>
+            <p>
+              <span>{footerInfo.phone || 'Your Phone'}</span>
+            </p>
+          </div>
+          {footerInfo.email && (
+            <div className={`flex items-center`}>
+              <i className="fa fa-envelope mr-2"></i>
+              <p>
+                <span>{footerInfo.email}</span>
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PreviewComponent;
--- src/pages/InfographicsDownloadPage.jsx
+++ src/pages/InfographicsDownloadPage.jsx
@@ -7,6 +7,7 @@ import { logo1, logo2, logo3 } from '../components/base64';
 import upload from '../assets/info/upload.svg';
 import Modal from 'react-modal';
 import Common from '../components/Sections/Common';
+import PreviewComponent from '../components/PreviewComponent';
 
 const colors = [
   { name: 'red', hex: '#f87171' },
@@ -22,6 +23,21 @@ const predefinedLogos = [
   { name: 'Logo 2', url: logo2 },
   { name: 'Logo 3', url: logo3 }
 ];
+
+const warnnings = {
+  name: 'Name can be upto 20 characters',
+  phone: 'Phone number can be upto 13 characters',
+  email: 'Email can only be upto 25 characters',
+  website: 'Website can only be upto 15 characters'
+}
+
+const limit = {
+  name: 30,
+  phone: 13,
+  email: 25,
+  website: 15
+
+}
 
 const InfographicDownloadPage = () => {
   const { id } = useParams();
@@ -46,20 +62,7 @@ const InfographicDownloadPage = () => {
     email: '',
     website: ''
   });
-  const warnnings = {
-    name: 'Name can be upto 20 characters',
-    phone: 'Phone number can be upto 13 characters',
-    email: 'Email can only be upto 25 characters',
-    website: 'Website can only be upto 15 characters'
-  }
 
-  const limit = {
-    name: 30,
-    phone: 13,
-    email: 25,
-    website: 15
-
-  }
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
@@ -144,7 +147,6 @@ const InfographicDownloadPage = () => {
             name: formData.get('name'),
             phone: formData.get('phone'),
             email: formData.get('email'),
-            website: formData.get('website'),
             isLogo: isLogo,
             logoBase64: logoBase64,
           },
@@ -193,113 +195,19 @@ const InfographicDownloadPage = () => {
   return (
     <>
       <div className="container mx-auto my-10 p-4">
-        <div className="container mx-auto p-4 flex flex-col lg:flex-row items-center lg:items-start">
-          <div className="w-full lg:w-1/2 ">
-            {/* <div className='box_scale'> */}
-            <div>
-
-              <img src={`https://utility.caclouddesk.com/uploads/${infographic.image}`} alt="Infographic" className="m-auto shadow-lg w-full md:w-[450px] " />
-              <div className='h-[60px] md:h-[80px] w-full md:w-[450px] m-auto flex justify-center items-center gap-x-[20px] p-1' style={{ background: selectedColor }}>
-                {selectedLogo || customLogo ? <div className="logo rounded-full overflow-hidden flex justify-center items-center bg-white">
-                  <img src={selectedLogo ? selectedLogo : customLogo ? URL.createObjectURL(customLogo) : "https://as2.ftcdn.net/v2/jpg/04/78/56/33/1000_F_478563312_HuepEVbPHRGC0dsbXOXL1YSuFIkWEm2m.jpg"} className='h-[40px] w-[50px] md:w-[70px] md:h-[70px] rounded-[100%]' alt="" />
-                </div> : ''}
-                <div className="info grid grid-cols-2 gap-x-5 gap-y-3 ">
-                  <div className="infowrap flex items-center">
-                    <svg
-                      width="15px"
-                      height="15px"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <circle cx="12" cy="9" r="3" stroke="#fff" stroke-width="1.5" />
-                      <circle cx="12" cy="12" r="10" stroke="#fff" stroke-width="1.5" />
-                      <path
-                        d="M17.9691 20C17.81 17.1085 16.9247 15 11.9999 15C7.07521 15 6.18991 17.1085 6.03076 20"
-                        stroke="#fff"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                      />
-                    </svg>
-                    <p className='text-[10px] md:text-[14px] text-white'>
-                      {formData.name || "Your Name"}
-                    </p>
-                  </div>
-                  <div className="infowrap flex items-center">
-                    <svg
-                      width="18px"
-                      height="18px"
-                      viewBox="0 -0.5 25 25"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M6.24033 8.16795C6.99433 7.37295 7.26133 7.14995 7.58233 7.04695C7.80482 6.98843 8.03822 6.98499 8.26233 7.03695C8.55733 7.12295 8.63433 7.18795 9.60233 8.15095C10.4523 8.99695 10.5363 9.08895 10.6183 9.25095C10.7769 9.54253 10.8024 9.88825 10.6883 10.1999C10.6043 10.4349 10.4803 10.5909 9.96533 11.1089L9.62933 11.4459C9.54093 11.5356 9.51997 11.6719 9.57733 11.7839C10.3232 13.0565 11.3812 14.1179 12.6513 14.8679C12.7978 14.9465 12.9783 14.921 13.0973 14.8049L13.4203 14.4869C13.6199 14.2821 13.8313 14.0891 14.0533 13.9089C14.4015 13.6935 14.8362 13.6727 15.2033 13.8539C15.3823 13.9379 15.4423 13.9929 16.3193 14.8669C17.2193 15.7669 17.2483 15.7959 17.3493 16.0029C17.5379 16.3458 17.536 16.7618 17.3443 17.1029C17.2443 17.2949 17.1883 17.3649 16.6803 17.8839C16.3733 18.1979 16.0803 18.4839 16.0383 18.5259C15.6188 18.8727 15.081 19.043 14.5383 19.0009C13.5455 18.9101 12.5847 18.6029 11.7233 18.1009C9.81416 17.0894 8.18898 15.6155 6.99633 13.8139C6.73552 13.4373 6.50353 13.0415 6.30233 12.6299C5.76624 11.7109 5.48909 10.6638 5.50033 9.59995C5.54065 9.04147 5.8081 8.52391 6.24033 8.16795Z"
-                        stroke="#fff"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                      <path
-                        d="M14.8417 4.29409C14.4518 4.15416 14.0224 4.35677 13.8824 4.74664C13.7425 5.1365 13.9451 5.56598 14.335 5.70591L14.8417 4.29409ZM18.7868 10.0832C18.9333 10.4707 19.3661 10.666 19.7536 10.5195C20.141 10.373 20.3364 9.94021 20.1899 9.55276L18.7868 10.0832ZM13.6536 6.52142C13.2495 6.43018 12.848 6.68374 12.7568 7.08778C12.6655 7.49182 12.9191 7.89333 13.3231 7.98458L13.6536 6.52142ZM16.5696 11.1774C16.6676 11.5799 17.0733 11.8267 17.4757 11.7287C17.8782 11.6307 18.125 11.2251 18.0271 10.8226L16.5696 11.1774ZM14.335 5.70591C16.3882 6.44286 18.0153 8.04271 18.7868 10.0832L20.1899 9.55276C19.2631 7.10139 17.3084 5.17942 14.8417 4.29409L14.335 5.70591ZM13.3231 7.98458C14.9238 8.34607 16.1815 9.58301 16.5696 11.1774L18.0271 10.8226C17.5042 8.67475 15.8098 7.0084 13.6536 6.52142L13.3231 7.98458Z"
-                        fill="#fff"
-                      />
-                    </svg>
-                    <p className='text-[10px] md:text-[14px] text-white'>
-                      {formData.phone || "Your Phone"}
-                    </p>
-                  </div>
-                  <div className="infowrap flex items-center">
-                    <svg
-                      width="15px"
-                      height="15px"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M3.02832 10L10.2246 14.8166C10.8661 15.2443 11.1869 15.4581 11.5336 15.5412C11.8399 15.6146 12.1593 15.6146 12.4657 15.5412C12.8124 15.4581 13.1332 15.2443 13.7747 14.8166L20.971 10M10.2981 4.06879L4.49814 7.71127C3.95121 8.05474 3.67775 8.22648 3.4794 8.45864C3.30385 8.66412 3.17176 8.90305 3.09111 9.161C3 9.45244 3 9.77535 3 10.4212V16.8C3 17.9201 3 18.4802 3.21799 18.908C3.40973 19.2843 3.71569 19.5903 4.09202 19.782C4.51984 20 5.07989 20 6.2 20H17.8C18.9201 20 19.4802 20 19.908 19.782C20.2843 19.5903 20.5903 19.2843 20.782 18.908C21 18.4802 21 17.9201 21 16.8V10.4212C21 9.77535 21 9.45244 20.9089 9.161C20.8282 8.90305 20.6962 8.66412 20.5206 8.45864C20.3223 8.22648 20.0488 8.05474 19.5019 7.71127L13.7019 4.06879C13.0846 3.68116 12.776 3.48735 12.4449 3.4118C12.152 3.34499 11.848 3.34499 11.5551 3.4118C11.224 3.48735 10.9154 3.68116 10.2981 4.06879Z"
-                        stroke="#fff"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
-                    <p className='text-[10px] md:text-[12px] text-white'>
-                      {formData.email || "Your Email"}
-                    </p>
-                  </div>
-                  <div className="infowrap flex items-center">
-                    <svg
-                      fill="#fff"
-                      width="12px"
-                      height="12px"
-                      viewBox="0 0 16 16"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M16 7.38A7.82 7.82 0 0 0 8 .5a7.82 7.82 0 0 0-8 6.88v1.24a7.82 7.82 0 0 0 8 6.88 7.82 7.82 0 0 0 8-6.88V7.38zm-1.25 0h-3a11.34 11.34 0 0 0-.43-2.54 7.6 7.6 0 0 0 1.75-1 6 6 0 0 1 1.65 3.54zm-9.18 0a9.69 9.69 0 0 1 .37-2.14A8.43 8.43 0 0 0 8 5.5a8.49 8.49 0 0 0 2.09-.26 10.2 10.2 0 0 1 .37 2.14zm4.92 1.24a9.59 9.59 0 0 1-.37 2.14 8.53 8.53 0 0 0-4.18 0 9.69 9.69 0 0 1-.37-2.14zm.4-5A11.82 11.82 0 0 0 10 2a6.89 6.89 0 0 1 2 1 6.57 6.57 0 0 1-1.14.66zm-2.6-1.86a10 10 0 0 1 1.38 2.3A7.63 7.63 0 0 1 8 4.25a7.56 7.56 0 0 1-1.67-.19 9.82 9.82 0 0 1 1.38-2.3h.58zm-3.15 1.9A6.57 6.57 0 0 1 4 3a6.89 6.89 0 0 1 2-1 10.38 10.38 0 0 0-.86 1.66zM3 3.83a7.6 7.6 0 0 0 1.75 1 11 11 0 0 0-.43 2.54h-3A6 6 0 0 1 3 3.83zM1.28 8.62h3a11 11 0 0 0 .43 2.54 7.6 7.6 0 0 0-1.75 1 6 6 0 0 1-1.68-3.54zm3.86 3.72A10.38 10.38 0 0 0 6 14a6.89 6.89 0 0 1-2-1 6.57 6.57 0 0 1 1.14-.66zm2.57 1.9a9.82 9.82 0 0 1-1.38-2.3 7.43 7.43 0 0 1 3.34 0 9.76 9.76 0 0 1-1.38 2.3h-.58zm3.15-1.9a6.57 6.57 0 0 1 1.19.66 7.24 7.24 0 0 1-2 1 11.48 11.48 0 0 0 .81-1.66zm2.14-.17a7.6 7.6 0 0 0-1.75-1 10.8 10.8 0 0 0 .43-2.54h3A6 6 0 0 1 13 12.17z"
-                      />
-                    </svg>
-                    <p className='text-[10px] md:text-[14px] text-white'>
-                      {formData.website || "Your Website"}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
+        <div className="container mx-auto p-4 flex flex-col lg:flex-row items-center justify-around">
+          <div className="w-full lg:w-1/2 flex justify-center items-center">
+            <PreviewComponent imgLink={infographic.image} footerInfo={formData} bgColor={selectedColor} logo={customLogo?URL.createObjectURL(customLogo):selectedLogo}/>
           </div>
           <div className="w-full lg:w-1/2 mt-4 lg:mt-0 lg:ml-4 p-4 bg-white rounded-md">
             <h1 className="text-2xl font-bold mb-4 capitalize">{infographic.title}</h1>
             <form onSubmit={handleSubmit}>
-              <div className='grid grid-cols-4 w-[80%] gap-y-3 grid-rows-4 justify-start font-manrope'>
+              <div className='grid grid-cols-4 w-[80%]  grid-rows-4 justify-start font-manrope items-center '>
                 <label className='col-span-1' htmlFor="name">Firm Name<span className='text-red-600'>*</span></label>
                 <div className="col-span-3">
                   <input
                     type="text"
-                    className='border border-gray-400 rounded-md ml-2 w-full'
+                    className='border border-gray-400 rounded-md ml-2 w-full p-1'
                     name='name'
                     value={formData.name}
                     onChange={handleChange}
@@ -313,7 +221,7 @@ const InfographicDownloadPage = () => {
                 <div className="col-span-3">
                   <input
                     type="text"
-                    className='border border-gray-400 rounded-md ml-2 w-full'
+                    className='border border-gray-400 rounded-md ml-2 w-full p-1 '
                     name="phone"
                     value={formData.phone}
                     onChange={handleChange}
@@ -327,27 +235,13 @@ const InfographicDownloadPage = () => {
                 <div className="col-span-3">
                   <input
                     type="email"
-                    className='border border-gray-400 rounded-md ml-2 w-full'
+                    className='border border-gray-400 rounded-md ml-2 w-full p-1'
                     name="email"
                     value={formData.email}
                     onChange={handleChange}
                     maxLength={limit.email}
                   />
                   {warnings.email && <span className='m-2 text-xs text-red-600'>{warnings.email}</span>}
-                </div>
-
-                <label className='col-span-1' htmlFor="website">Website</label>
-                <div className="col-span-3">
-                  <input
-                    type="text"
-                    className='border border-gray-400 rounded-md ml-2 w-full'
-                    name="website"
-                    value={formData.website}
-                    onChange={handleChange}
-                    maxLength={limit.website + 1}
-
-                  />
-                  {warnings.website && <span className=' m-2 text-xs text-red-600'>{warnings.website}</span>}
                 </div>
                 <label className=' col-span-1' htmlFor="logo">Logo Upload</label>
                 <div className='col-span-3 flex items-center gap-5 flex-wrap '>
